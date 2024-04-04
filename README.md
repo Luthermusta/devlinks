ad6190ba5bfaebfee07142aa24e290dd6fcdc0b6
luthermusta <mustaluther18@gmail.com>
2024-04-03 23:56:52 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Luther-Loren-o
---
+++ Visual code/script.js
@@ -0,0 +1,23 @@
+function toggleMode() {
+
+const html= document.documentElement
+const img= document.queryselector("#profile img")
+
+if(ReportBody.classlist.contains('light')){
+    html.classLight.remove
+    img.setAtribute("src" ./assets/avatar-light.png")
+
+}
+
+else{
+  
+    html.classLight.add('light')
+img.setAtribute("src" "./assets/avatar.png")
+}
+
+}
+
+const img= document.queryselector("#profile img")
+
+
+
